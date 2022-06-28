d0fd80d450d2a36e1f7beea2f4ee416c94d99052
Даниил Маслов <danyamas02@mail.ru>
2022-06-28 13:06:35 +0300

    Frontend
    Frontend

---
+++ main.js
@@ -0,0 +1,9 @@
+let switchMode = document.getElementById("switchMode");
+switchMode.onclick = function () {
+  let theme = document.getElementById("theme");
+  if (theme.getAttribute("href") == "dark.css") {
+    theme.href = "light.css";
+  } else {
+    theme.href = "dark.css";
+  }
+};
